88fd4a3ca383c619f2a970a676ab486bfda29d0c
xenosign <xenosign@naver.com>
2022-10-19 15:19:37 +0900

    회원 가입 페이지 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Index from './pages/Index';
 import Item from './pages/Item';
 import Join from './pages/Join';
 import Login from './pages/Login';
+import Signup5 from './pages/Signup5';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/item" element={<Item />} />
       <Route path="/join" element={<Join />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup5" element={<Signup5 />} />
     </Routes>
   );
 }
