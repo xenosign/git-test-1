d4ccdc154e1b30fbf0e1be08214a5811192f6a54
bogus <qazxwd44@hanmail.net>
2022-10-19 15:30:28 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { Route, Routes } from 'react-router-dom';
-import Index from './pages/Index';
-import Item from './pages/Item';
-import Join from './pages/Join';
-import Login from './pages/Login';
-import Signup5 from './pages/Signup5';
+import { Route, Routes } from "react-router-dom";
+import Index from "./pages/Index";
+import Item from "./pages/Item";
+import Join from "./pages/Join";
+import Login from "./pages/Login";
+import Signup5 from "./pages/Signup5";
 import Signup4 from "./pages/Signup4";
 
 function App() {
