dda26f19722e7e3de45baedbe0d229847342f994
kdtTetz <xenosign@naver.com>
2023-02-27 20:41:37 +0900

    a

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,6 +1,6 @@
-import Header from '../components/Header';
-import styled from 'styled-components';
-import Footer from '../components/Footer';
+import Header from "../components/Header";
+import styled from "styled-components";
+import Footer from "../components/Footer";
 
 const Main = styled.main`
   text-align: center;
@@ -16,6 +16,7 @@ export default function Index() {
     <Main>
       <Header />
       <Header />
+      <MyImg src="https://i1.sndcdn.com/avatars-uubmOX0QQigzrI2m-5wHwlA-t240x240.jpg" />
       <MyImg src="https://i1.sndcdn.com/avatars-uubmOX0QQigzrI2m-5wHwlA-t240x240.jpg" />
       <Footer />
     </Main>
